b4dce2c92551a2920c0f185ddd7ccd51a805e9ca
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-30 00:17:37 +0600

    softmax classifier
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

---
+++ Linear Image Classifier/SotfmaxClassifier.py
@@ -0,0 +1,9 @@
+import random
+import numpy as np
+from cs231n.data_utils import load_CIFAR10
+import matplotlib.pyplot as plt
+
+from __future__ import print_function
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
